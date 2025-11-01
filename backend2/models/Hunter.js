ee46d501d18b137c0bc53d6f538f8b8eb7d34afe
Carlos Mauricio Martinez <maovegeta@gmail.com>
2025-11-01 10:13:19 -0500

    Segundo commit

--- backend2/models/Hunter.js
+++ backend2/models/Hunter.js
@@ -26,42 +26,4 @@ const hunterSchema = new mongoose.Schema(
 
 export const Hunter = mongoose.model('Hunter', hunterSchema, 'hunters');
 
-const CaballeroSchema = new mongoose.Schema(
-  {
-    nombre: {
-      type: String,
-      required: [true, "El nombre es obligatorio"],
-      trim: true,
-    },
-    constelacion: {
-      type: String,
-      required: [true, "La constelación es obligatoria"],
-      trim: true,
-    },
-    nivel: {
-      type: String,
-      required: [true, "El nivel es obligatorio"],
-      enum: ["Bronce", "Plata", "Oro"], // valores válidos
-    },
-    descripcion: {
-      type: String,
-      required: [true, "La descripción es obligatoria"],
-      trim: true,
-    },
-    imagen: {
-      type: String,
-      required: [true, "La URL de la imagen es obligatoria"],
-      validate: {
-        validator: (url) =>
-          /^(https?:\/\/.*\.(?:png|jpg|jpeg|webp|gif|svg))$/i.test(url),
-        message: "La URL de la imagen no es válida o no apunta a una imagen",
-      },
-    },
-  },
-  { timestamps: true, 
-    versionKey: false, // elimina el campo __v
-    collection: 'cualities', // usar la colección 'cualities' en la base de datos
-    }
-);
 
-export const Caballero = mongoose.model("Caballero", caballeroSchema, 'cualities');
